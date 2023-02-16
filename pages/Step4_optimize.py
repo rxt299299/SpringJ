86e49f5d8fc61da7ccf26df96b453423e2a67b87
rxt299299 <rxt299299@gmail.com>
2023-02-15 17:22:32 -0800

    change scipydirect minimize path
    change scipydirect minimize path

--- pages/Step4_optimize.py
+++ pages/Step4_optimize.py
@@ -12,7 +12,7 @@ from streamlit_extras.switch_page_button import switch_page
 
 
 from SpringDesign.spring_design import *
-from Scipydirect.scipydirect import minimize
+from Scipydirect import minimize
 import plotly.express as px
 import pandas as pd
 import time
